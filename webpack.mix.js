fbc86f03e9b9726b54d6a017701361549fd5a953
xTrimy <Mohamed.ashraf881999@gmail.com>
2021-08-15 02:01:14 +0200

    Main Navigation Bar (Header)

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,10 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require("tailwindcss"),
     ]);
-
-mix.js("resources/js/app.js", "public/assets/js")
-.postCss("resources/css/app.css", "public/assets/css/master.css", [
-    require("tailwindcss"),
-]);
