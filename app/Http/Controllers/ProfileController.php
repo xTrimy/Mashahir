ff76bb9e1f3513b68466e2d08871db94ba9076f3
Mohammad Salah <40919421+SherlokMo@users.noreply.github.com>
2021-08-23 04:37:59 +0200

    Dynamic user header and profile

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\profilemiddleware;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+
+
+    public function index($username)
+    {
+
+        $profile = User::select('name', 'image')->where('username', '=', $username)->first();
+
+        return view('pages.profile', $profile);
+    }
+
+}
