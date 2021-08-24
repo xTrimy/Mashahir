8e07c80805a4dfc5221f853fafb9d4b04c9e1547
Abdelrahman Sayed <61325578+xBodda@users.noreply.github.com>
2021-08-24 03:23:23 +0200

    requests

--- resources/views/dashboard/requests.blade.php
+++ resources/views/dashboard/requests.blade.php
@@ -49,8 +49,23 @@
                 </div>
             </div>
 
-            <div class="w-full mt-8 bg-white shadow-lg px-12 py-8 rounded-sm">
-                
+            <div class="w-full mt-8 bg-white shadow-lg px-12 py-4 rounded-md border-r-12 border-pink-700">
+                <div class="flex items-center">
+                    <div class="w-20 h-20 mr-2 md:ml-10 relative">
+                        <img class="w-full h-full object-cover rounded-full" src="{{ asset('image/placeholders/face-3.jpg') }}"/>
+                        <img class="w-3/6 h-2/4 object-cover rounded-full absolute bottom-0 right-0" src="{{ asset('image/placeholders/face-4.jpg') }}"/>
+                    </div>
+                    <div>
+                        <b class="text-sm md:text-xl font-medium">إعلان دعائي لمجلة سيدتي </b>
+                        <div class="flex">
+                            <p class="mt-0 md:mt-2 text-xs md:text-base text-gray-400"> <i class="fas fa-user"></i> حلا الترك </p>
+                            <p class="mt-0 md:mt-2 text-xs md:text-base text-gray-400 mr-5"> <i class="fas fa-user ml-1"></i> Ibrahim Ashraf</p>
+                            <p class="mt-0 md:mt-2 text-xs md:text-base text-gray-400 mr-5"> <i class="fas fa-dollar-sign ml-1"></i> 780.00</p>
+                            <p class="mt-0 md:mt-2 text-xs md:text-base text-pink-700 mr-5"> <i class="fas fa-exclamation-circle ml-1"></i> جاري التنفيذ </p>
+                            <p class="mt-0 md:mt-2 text-xs md:text-base text-gray-400 mr-5"> <i class="fas fa-clock ml-1"></i> منذ يوم وساعة </p>
+                        </div>
+                    </div>
+                </div>
             </div>
             
         </div>
